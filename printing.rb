caeacc926018b5ab3f09248fb0ac7233dc197ebc
ReggieNora <reggieNora@github.com>
2018-12-09 16:45:27 -0500

    Finished exercise on 12/9/18

---
+++ printing.rb
@@ -0,0 +1,29 @@
+# This line prints the first line
+puts "Mary had a little lamb."
+
+# This line describes what Mary's lamb looked like.
+puts "It's fleece was white as #{'snow'}."
+
+# This line talks avout how the lamb followed Mary. 
+puts "And everywhere that Mary went."
+
+# What did that do?
+puts "." * 10 # This created a divider between the statements using the "." character. 
+
+# This provides all the letters for "Cheeseburger"
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line and what's it do? 
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
