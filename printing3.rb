b5e1b7143b69983947053b9b6ebcbdeeb49ea61a
ReggieNora <reggieNora@github.com>
2018-12-25 16:35:27 -0500

    Completed exercise on 12/25

--- printing3.rb
+++ printing3.rb
@@ -5,7 +5,7 @@ formatter = "%{first} %{second} %{third} %{fourth}"
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 
 #This one will print out the string values in the formatter
-puts formatter % {first: "one", second: "two", third: "three" fourth: "four"}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
 
 #This will output the boolean values in formatter
 puts formatter % {first: true, second: false, third: true, fourth: false} 
